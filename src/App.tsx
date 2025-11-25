4d482aa6ae18e79c14811228efb07fbf559eb7ed
SecureYou <secureyou@example.com>
2025-11-26 03:29:33 +0600

    Remove onboarding page - redirect directly to login

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { ProtectedRoute } from "@/components/ProtectedRoute";
 import { InstallPWA } from "@/components/InstallPWA";
 import { Component, ReactNode } from "react";
 import Splash from "./pages/Splash";
-import Onboarding from "./pages/Onboarding";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ResetPassword from "./pages/ResetPassword";
@@ -86,7 +85,6 @@ const App = () => {
             <AuthProvider>
               <Routes>
                 <Route path="/" element={<Splash />} />
-                <Route path="/onboarding" element={<Onboarding />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
